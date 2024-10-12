7aad741e818b31cf4d53fa2f0f8e5b4e4b7a7d60
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2024-10-12 23:51:48 +0700

    Membuat fungsi copy dengan perubahan

--- app/src/main/java/com/example/questbasickotlin_0125/DataClass.kt
+++ app/src/main/java/com/example/questbasickotlin_0125/DataClass.kt
@@ -21,6 +21,10 @@ fun main() {
     //fungsi copy
     val data3 = data.copy()
     println(data3)
+
+    //fungsi copy dengan perubahan
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
 }
 
 
