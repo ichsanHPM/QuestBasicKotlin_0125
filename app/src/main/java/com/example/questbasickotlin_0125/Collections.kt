3024b95f1c6b0d9f21c5ba483b498e0682deaea7
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2024-10-12 23:02:04 +0700

    Set beserta contohnya

--- app/src/main/java/com/example/questbasickotlin_0125/Collections.kt
+++ app/src/main/java/com/example/questbasickotlin_0125/Collections.kt
@@ -32,3 +32,33 @@ fun ContohList() {
     val shapesLocked: List<String> = shape
     println(shapesLocked)
 }
+
+//Set adalah kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik(tidak ada duplikasi)
+//Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+//Set bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+//Set Read-only menggunakan setof
+//Set Mutable menggunakan mutableSetof
+
+fun ContohSet() {
+    println()
+    println("=== Set ===")
+
+    //Set Read-Only
+    val readOnlyAbjad = setOf("A", "B","C")
+    println(readOnlyAbjad)
+
+    //Set Mutable
+    val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    //Menambahkan data ke dalam Set Mutable
+
+
+    //Menghapus data dari Set Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    //Set Read-Only
+    val shapesLocked: Set<String> = shape
+    println(shapesLocked)
+}
