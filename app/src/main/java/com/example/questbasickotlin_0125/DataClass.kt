64fc27b29caaaa594bf7e3e9ecb3af9511d20419
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2024-10-12 23:43:03 +0700

    Membuat data class

---
+++ app/src/main/java/com/example/questbasickotlin_0125/DataClass.kt
@@ -0,0 +1,18 @@
+package com.example.questbasickotlin_0125
+
+//Data class
+//Data class adalah class yang digunakan untuk menyimpan data
+//Data class menyediakan fungsi untuk meng-override fungsi equals(), hashcode(), dan toString()
+//Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan
+
+data class DataClass(
+    val id: Int,
+    var email: String
+)
+
+fun main() {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+
+}
