edf5d1de7c7ad72d3464da90d679cc83cc6ec7b4
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2024-10-13 00:52:32 +0700

    menyelesaikan null safety

---
+++ app/src/main/java/com/example/questbasickotlin_0125/NullSafety.kt
@@ -0,0 +1,37 @@
+package com.example.questbasickotlin_0125
+
+fun nullSafety() {
+    //neverNull has string type
+    var neverNull: String = "This can't be null"
+
+    //throws a compiler error
+    //neverNull = null
+
+    //nullable has nullable string type
+    var nullable: String?= "You can keep a null here"
+    //this is ok
+    nullable = null
+
+    //check for nul in conditions
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+
+    //safe call operator
+    println(neverNull.length) //18
+    println(nullable?.length) //null
+
+    //notNull dosn't accept null values
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+
+    println(strLength(neverNull)) //18
+
+}
+
+fun main() {
+    nullSafety()
+}
