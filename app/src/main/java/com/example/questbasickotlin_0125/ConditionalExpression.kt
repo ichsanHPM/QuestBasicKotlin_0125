debd341eb89047464ccc31abe71ce48db1f8cd12
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2024-10-12 23:10:18 +0700

    Conditional Expression menggunakan if-else

---
+++ app/src/main/java/com/example/questbasickotlin_0125/ConditionalExpression.kt
@@ -0,0 +1,16 @@
+package com.example.questbasickotlin_0125
+
+//Conditional Expression
+//Conditional Expression adalah ekspressi yang mengembalikan nilai berdasarkan kondisi tertentu
+//Conditional Expression menggunakan if-else, when, dan try-catch
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+    //IF-else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+}
